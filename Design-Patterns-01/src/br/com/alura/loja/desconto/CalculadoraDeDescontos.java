cbc2c2298f07222c95e2cabb9897ecf659b9ef66
Celso Ribeiro <celsofilho82@gmail.com>
2021-03-01 17:41:51 -0300

    Utilizando o padrão Chain of Responsibility para resolver o cálculo dos descontos

--- Design-Patterns-01/src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
+++ Design-Patterns-01/src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
@@ -7,15 +7,23 @@ import br.com.alura.loja.orcamento.Orcamento;
 public class CalculadoraDeDescontos {
 
 	public BigDecimal calcular(Orcamento orcamento) {
-		if (orcamento.getQuantidadeItens() > 5) {
-			return orcamento.getValor().multiply(new BigDecimal("0.1"));
-		}
 
-		if (orcamento.getValor().compareTo(new BigDecimal("500")) > 0) {
-			return orcamento.getValor().multiply(new BigDecimal("0.1"));
-		}
+		/**
+		 * No caso do cálculo de descontos não sabemos qual desconto vai ser aplicado no
+		 * orçamento portanto, precisamos verificar todos os descontos possíveis de
+		 * serem aplicados e caso o desconto em questão não seja aplicado é preciso
+		 * chamar o próximo desconto seguindo uma cadeia. Implementamos a classe
+		 * SemDesconto, que é responsável por finalizar essa cadeia de
+		 * responsabilidades.
+		 * 
+		 * Chain of Responsibility, padrão para aplicar um desconto dentro de uma cadeia
+		 * de possíveis descontos.
+		 */
 
-		return BigDecimal.ZERO;
+		Desconto desconto = new DescontoParaOrcamentoComMaisDeCincoItens(
+				new DescontoParaOrcamentoComValorMaiorQueQuinhentos(new SemDesconto()));
+
+		return desconto.calcular(orcamento);
 	}
 
 }
