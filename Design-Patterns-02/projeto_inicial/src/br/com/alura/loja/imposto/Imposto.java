a0aaa7c68d6453ffaa6063f02b15138e804dbd74
Celso Ribeiro <celsofilho82@gmail.com>
2021-03-03 18:09:03 -0300

    Resolvendo o problema da combinação de Impostos com o padrão Decorator

--- Design-Patterns-02/projeto_inicial/src/br/com/alura/loja/imposto/Imposto.java
+++ Design-Patterns-02/projeto_inicial/src/br/com/alura/loja/imposto/Imposto.java
@@ -4,8 +4,41 @@ import java.math.BigDecimal;
 
 import br.com.alura.loja.orcamento.Orcamento;
 
-public interface Imposto {
+public abstract class Imposto {
 
-	BigDecimal calcular(Orcamento orcamento);
+	/*
+	 * Decorator: Neste exemplo a classe Imposto tem como composição ela mesma ou
+	 * seja tem um auto relacionamento com ela mesma (Decoração).
+	 * 
+	 * Utilizamos o padrão Decorator para resolver um problema em específico da
+	 * aplicação. Flexibilizar, de maneira dinâmica, o cálculo de diferentes
+	 * impostos de um orçamento.
+	 * 
+	 * O padrão Decorator permite adicionar novos comportamentos a um objeto,
+	 * tornando o código bastante flexível e dinâmico.
+	 * 
+	 * O padrão Decorator é muito poderoso e bastante comum de ser implementado, mas
+	 * possui alguns detalhes importantes a serem observados, como o fato do
+	 * Decorator precisar possuir a mesma interface do objeto que ele está
+	 * decorando.
+	 */
+
+	private Imposto outroImposto;
+
+	public Imposto(Imposto outroImposto) {
+		this.outroImposto = outroImposto;
+	}
+
+	protected abstract BigDecimal realizarCalculo(Orcamento orcamento);
+
+	public BigDecimal calcular(Orcamento orcamento) {
+		BigDecimal valorImposto = realizarCalculo(orcamento);
+		BigDecimal valorOutroImposto = BigDecimal.ZERO;
+		if (outroImposto != null) {
+			valorImposto = outroImposto.realizarCalculo(orcamento);
+		}
+
+		return valorImposto.add(valorOutroImposto);
+	}
 
 }
