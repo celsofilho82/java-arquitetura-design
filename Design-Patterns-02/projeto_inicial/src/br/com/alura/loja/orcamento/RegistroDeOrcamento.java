258eb4fd3968a04c13f272bdc87f292a5897d612
Celso Ribeiro <celsofilho82@gmail.com>
2021-03-03 10:50:11 -0300

    Problema: Necessitamos chamar uma API externa para registro de orçamento

---
+++ Design-Patterns-02/projeto_inicial/src/br/com/alura/loja/orcamento/RegistroDeOrcamento.java
@@ -0,0 +1,13 @@
+package br.com.alura.loja.orcamento;
+
+public class RegistroDeOrcamento {
+
+	/*
+	 * Essa classe tem como objetivo chamar uma API externa para registrar um
+	 * orçamento
+	 */
+	public void registrar(Orcamento orcamento) {
+
+	}
+
+}
